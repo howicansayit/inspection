dd646a70b2285c2c00035c00a0d1908c66a5c517
howicansayit <howicansayit>
2022-10-15 23:03:05 +0300

    fix

--- reports.js
+++ reports.js
@@ -78,7 +78,7 @@ function showReportsPage() {
                     console.log(data[i])
 
                     let sendDate = new Date(data[i].timestamp)
-                    sendDate = `${('0' + sendDate.getHours()).slice(-2)}:${('0' + sendDate.getMinutes()).slice(-2)} ${('0' + sendDate.getDate()).slice(-2) + '/'
+                    sendDate = `${('0' + sendDate.getHours()).slice(-2)}:${('0' + sendDate.getMinutes()).slice(-2)}:${('0' + sendDate.getSeconds()).slice(-2)} ${('0' + sendDate.getDate()).slice(-2) + '/'
                         + ('0' + (sendDate.getMonth() + 1)).slice(-2) + '/'
                         + sendDate.getFullYear()}`
 
